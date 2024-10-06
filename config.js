209622305bdf589676534d444ec11d81a3ec9ac2
upanshu <upanshu21@gmail.com>
2024-10-06 11:18:29 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,101 @@
+window.PorfolioConfig = {
+  name: 'Upanshu Chaudary',
+  jobTitle: 'Software Engineer',
+  jobDescription: 'Hey There! Welcome to my Portfolio. I am a Software Engineer with 3 years of Professional Experience. I bring a strong knowledge of Software Engineering and a Product mindset. Lets Connect!',
+  skills: [
+    {
+      name: 'Frontend Development',
+      description: 'JavScript, React, HTML5, TailwindCSS, MaterialUI'
+    },
+    {
+      name: 'Backend Development',
+      description: 'Java, Spring Boot, Python, Django, NodeJS, Express, MongoDB, PosgreSQL, Redis, RESTful APIs'
+    },
+    {
+      name: 'DevOps/Infrastcture',
+      description: 'Docker, Kubernetes, Jenkins, Git, GitHub, AWS, GCP, Concourse, Trraform, Ansible, Linux, ElasticSearch, Kibana'
+    }
+  ],
+  works: [
+    {
+      name: 'Application tracker chrome-extension / JavaScript, HTML, Python, Web scrapping',
+      link: 'https://github.com/upanshu21/chrome-extension',
+      description: 'Created extension to keep track of job applications and suggest relevant mutual connections. Integrated with LinkedIn APIs to fetch job details.'
+    },
+    {
+      name: 'Apple gRPC Protobuf Wrapper / GoLang, Swift, gRPC, Protobuf',
+      link: 'https://github.com/upanshu21',
+      description: 'Ideated and Engineered with Professor Will a solution to provide a service API to implement gRPC methods from proto file. Project belongs to OSU.'
+    },
+    {
+      name: 'Local-stack local cloud development / Java, Kotlin, Maven, AWS, Dynamo, Travis CI, Docker',
+      link: 'https://github.com/localstack/localstack-java-utils/pull/35',
+      description: 'Implemented support features for localstack which is a localized AWS environment where developers can build and test apps. Added support for DynamoDBAsyncClient required for KCL 2.0x to deploy and test AWS Kinesis Locally.'
+    },
+    {
+      name: 'Cowin-Vaccination tracker / Python, Django, Mailgun, Telegram API, Scheduler, AWS EC2, Docker',
+      link: 'https://github.com/upanshu21/cowin-vaccine-tracker',
+      description: 'Developed an application to monitor and book COVID-19 vaccine slots, notifying 20 active users via email and Telegram.Implemented features allowing users to select their preferred geographical region, location, minimizing travel distance.'
+    },
+    {
+      name: 'Yoga-AI / Flutter, Dart, PoseNet Model, Tensorflow Lite, Firebase, Android Studio',
+      link: 'https://github.com/upanshu21',
+      description: 'Developed a Health Care app using PoseNet to detect human poses and a text to speech engine to speak to user, emulating the role of a Yoga instructor. Project Showcased as Final year project for Undergrad receiving Highest score in batch.'
+    },
+    {
+      name: 'canvas-orchestrator / Python, FastAPI, Docker, MiniKube, Google Kubernetes Engine',
+      link: 'https://github.com/upanshu21/canvas-orchestration',
+      description: 'Developed and deployed full-stack canvas clone on GKE cluster. Provisioned Infrastructre, created services for backend and front-end communication and provided a public IP for access.'
+    }
+    ,
+    {
+      name: 'Leo Messi Homepage/ HTML, CSS',
+      link: 'https://github.com/upanshu21/project_uc',
+      description: 'My first HTML and CSS project. A simple homepage for Leo Messi. Brings back memories.'
+    }
+  ],
+  Blogs: [
+    {
+      name: 'Migrating secrets using HashiCorp Vault and safe CLI',
+      link: 'https://upanshu73.medium.com/migrating-secrets-using-hashicorp-vault-and-safe-cli-7f76a7dc7907',
+      description: 'A Step by Step guide to migrate secrets from one vault to another using Hashicorp vault and Safe CLI'
+    },
+    {
+      name: 'Understanding Kubernetes',
+      link: 'https://upanshu73.medium.com/understanding-kubernetes-480ff4e11a89',
+      description: 'A beginner guide to understand Kubernetes and its components.'
+    },
+    {
+      name: 'Multithreading in Java',
+      link: 'https://upanshu73.medium.com/multithreading-in-java-150fc67fefc0',
+      description: 'A detailed guide to understanding multithreading in Java.'
+    },
+    {
+      name: 'Singleton Design Pattern',
+      link: 'https://upanshu73.medium.com/singleton-design-pattern-bc825a0b6b76',
+      description: 'A detailed guide to understanding Singleton Design Pattern.'
+    },
+    {
+      name: 'Circuit Breaker Pattern With Netflix-Hystrix: Java',
+      link: 'https://upanshu73.medium.com/circuit-breaker-pattern-with-netflix-hystrix-java-f9bed81706be',
+      description: 'A detailed guide to understanding Circuit Breaker Pattern with Netflix-Hystrix in Java.'
+    }
+  ],
+  contacts: {
+    email: 'upanshu21@gmail.com',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/upanshu21'
+      },
+      {
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/upanshuch/'
+      },
+      {
+        name: 'Medium',
+        link: 'https://upanshu73.medium.com/'
+      }
+    ]
+  }
+};
